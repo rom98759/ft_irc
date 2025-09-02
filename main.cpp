620d67eeec617090d7959c9bda0f2e53aa90f349
rom98759 <romain479536@gmail.com>
2025-09-02 17:09:05 +0200

    Ajouter import de server.hpp et client.hpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@
 /*   By: rcaillie <rcaillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 12:02:16 by kzhen-cl          #+#    #+#             */
-/*   Updated: 2025/09/02 17:06:08 by rcaillie         ###   ########.fr       */
+/*   Updated: 2025/09/02 17:08:00 by rcaillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/socket.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "Server.hpp"
+#include "Client.hpp"
 
 static unsigned short	ft_atous(const char *const a)
 {
